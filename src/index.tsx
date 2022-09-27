d8e9980e6a9a83bd5e802ce7b703b0888950b8ef
ivan-kramarenko <i-v-kr@yandex.ru>
2022-09-27 13:20:19 +0300

    Added bootstrap import

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import 'bootstrap/dist/css/bootstrap.css'
 
 const root = createRoot(document.querySelector('#root') as HTMLElement)
 root.render(<App />)
