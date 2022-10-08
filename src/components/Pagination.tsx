a503786a175c32dee0457627f9fc6f1b7ca0166c
ivan-kramarenko <i-v-kr@yandex.ru>
2022-10-08 16:29:01 +0300

    pagination (without switch)

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import _ from 'lodash'
+
+interface PaginationProps {
+  itemsCount: number
+  pageSize: number
+  onPageChange: (e: React.MouseEvent<HTMLSpanElement>, pageIndex: number) => void
+  currentPage: number
+}
+
+const Pagination = ({ itemsCount, pageSize, onPageChange, currentPage }: PaginationProps) => {
+  const pageCount: number = Math.ceil(itemsCount / pageSize)
+  if (pageCount === 1) return null
+  const pages: Array<number> = _.range(1, pageCount + 1)
+
+  const setAnchorClassname = (page: number): string => {
+    const headerIsActive = currentPage === page ? 'active' : ''
+    const anchorClasses = ['page-link', headerIsActive]
+    return anchorClasses.join(' ')
+  }
+  return (
+    <>
+      <nav>
+        <ul className="pagination">
+          {pages.map((page) => (
+            <li key={`page_${page}`} className="page-item">
+              <a className={setAnchorClassname(page)} onClick={(e) => onPageChange(e, page)}>
+                {page}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </>
+  )
+}
+
+export default Pagination
