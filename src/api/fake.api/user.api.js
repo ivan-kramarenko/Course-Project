8e29aa35475106a98ee510275e1026d8f97512f8
ivan-kramarenko <i-v-kr@yandex.ru>
2022-10-27 21:32:15 +0300

    hotfix

--- src/api/fake.api/user.api.js
+++ src/api/fake.api/user.api.js
@@ -11,7 +11,11 @@ const qualities = {
     name: 'Странный',
     color: 'secondary'
   },
-  buller: { _id: '67rdca3eeb7f6fgeed4711012', name: 'Троль', color: 'success' },
+  buller: {
+    _id: '67rdca3eeb7f6fgeed4711012',
+    name: 'Тролль',
+    color: 'success'
+  },
   alcoholic: {
     _id: '67rdca3eeb7f6fgeed471101',
     name: 'Алкоголик',
